81a128b0c2f3e8f9c5878267d52b15f154d28e79
Ravi Kumar <rk181997@gmail.com>
2016-10-20 19:10:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var content = {
+var articleOne = {
     title:'article one | Ravi K',
     heading:'articel one',
     date:'oct 20 2016 ',
@@ -37,7 +37,11 @@ var content = {
             
 };
 
-
+function createTemplate (data) {
+    var title= data.title;
+    var date= data.date;
+    var heading= data.heading;
+    var content= data.content;
 var htmlTemplate =`
 <html>
      <head>
@@ -63,13 +67,15 @@ var htmlTemplate =`
          </div>
      </body>
  </html> `;
+ return htmltemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get("/article-one", function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get("/article-two", function(req, res){
